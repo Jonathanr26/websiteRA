b82587f12a13364d97c483fb81fe8333eef65d41
Jonathanr26 <jrodriguez110@ucol.mx>
2022-06-01 02:25:51 -0500

    QNEXOS

---
+++ src/mian.js
@@ -0,0 +1,8 @@
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll("a");
+
+for (let i = 0; i < menuItem.length; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active";
+  }
+}
